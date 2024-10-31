26da5d523f361800eb95a6c1143ee54615032d3f
AkifAhmed5718 <122450551+AkifAhmed5718@users.noreply.github.com>
2024-10-31 22:07:21 +0100

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import { ColorSchemesSwitcher } from "@/components/color-schemes-switcher";
+import Header from "@/components/header/Header";
+import {
+  AppShell,
+  AppShellHeader,
+  AppShellMain,
+  Group,
+  Text,
+  Title,
+} from "@mantine/core";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
